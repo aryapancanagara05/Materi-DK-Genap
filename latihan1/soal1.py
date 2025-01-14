127e975adca1a8931fe736f5b30d2761fc264df1
aryapancanagara05 <aryapancanagara78@gmail.com>
2025-01-14 07:49:34 +0700

    menambahkan soal1

---
+++ latihan1/soal1.py
@@ -0,0 +1,7 @@
+#program untuk menentukan bilangan kelipatan empat
+bilangan=int(input("masukan bilangan :"))
+
+if bilangan % 4 == 0:
+    print(bilangan,"kelipatan 4")
+else:
+    print(bilangan,"bukan kelipatan 4")
