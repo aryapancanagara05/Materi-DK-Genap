af3316796135a2b4043c30f80992401efc7b33c6
fahrizalrakhagunawan <agoentea30@gmail.com>
2025-02-11 07:44:20 +0700

    menambahkan folder baru

---
+++ materi_fungtion/fungtion.py
@@ -0,0 +1,10 @@
+def cetakidentitassiswa(nama,kelas,jenis_kelamin):
+       print("nama :",nama)
+       print("kelas :",kelas)
+       print("jenis kelamin :",jenis_kelamin)
+       
+#panggil fungsi cetak identitas siswa
+cetakidentitassiswa("john","x-ppl","laki-laki")
+cetakidentitassiswa("dimas","x-ppl","laki-laki")
+cetakidentitassiswa("aldi","x-ppl","laki-laki")
+cetakidentitassiswa("agus","x-ppl","laki-laki")
